a3912d339b0f527b17c05ab16e1ee3f13869b5b3
박세은 <seeunbag30@gmail.com>
2023-11-27 00:12:09 +0900

    Fix code : animate.css 라이브러리 사용해서 애니메이션 수정

--- src/Home.js
+++ src/Home.js
@@ -30,20 +30,20 @@ function Home() {
       archivingRef.current?.scrollIntoView({ behavior: 'smooth' });
     };
 
-    const [showNav, setShowNav] = useState(false);
-    const [VisibleHeader, setVisibleHeader] = useState(false);
-    const [VisibleContents, setVisibleContents] = useState(false);
+    const [aboutShowNav, setaboutShowNav] = useState(false);
+    const [aboutVisibleHeader, setaboutVisibleHeader] = useState(false);
+    const [aboutVisibleContents, setaboutVisibleContents] = useState(false);
 
 
     const handleScroll = () => {
       const aboutPosition = aboutRef.current.offsetTop;
-      setShowNav(window.scrollY >= aboutPosition - 1);
+      setaboutShowNav(window.scrollY >= aboutPosition - 1);
   
-      const onScreenHeader = window.scrollY  > aboutPosition - 300;
-      setVisibleHeader(onScreenHeader);
+      const onScreenAboutHeader = window.scrollY  > aboutPosition - 300;
+      setaboutVisibleHeader(onScreenAboutHeader);
 
-      const onScreenContents = window.scrollY  > aboutPosition - 1;
-      setVisibleContents(onScreenContents);
+      const onScreenAboutContents = window.scrollY  > aboutPosition - 1;
+      setaboutVisibleContents(onScreenAboutContents);
   };
 
     useEffect(() => {
@@ -61,8 +61,8 @@ function Home() {
               </div>
           </section>
           <NavbarStuck onHomeClick={scrollToHome} onAboutClick={scrollToAbout} onPortfolioClick={scrollToportfolio} onExperiencesClick={scrollToexperiences} onArchivingClick={scrollToarchiving}/>
-          <Navbar onHomeClick={scrollToHome} onAboutClick={scrollToAbout} onPortfolioClick={scrollToportfolio} onExperiencesClick={scrollToexperiences} onArchivingClick={scrollToarchiving} show={showNav}/>
-          <About ref={aboutRef} visibleHeader={VisibleHeader} visibleContents={VisibleContents} />
+          <Navbar onHomeClick={scrollToHome} onAboutClick={scrollToAbout} onPortfolioClick={scrollToportfolio} onExperiencesClick={scrollToexperiences} onArchivingClick={scrollToarchiving} show={aboutShowNav}/>
+          <About ref={aboutRef} aboutVisibleabHeader={aboutVisibleHeader} aboutVisibleContents={aboutVisibleContents} />
           <Portfolio ref={portfolioRef}/>
           <Experiences ref={experiencesRef}/>
           <Archiving ref={archivingRef}/>
--- src/about.js
+++ src/about.js
@@ -3,23 +3,23 @@ import './style/about.css';
 import 'animate.css';
 import Skills from './skills';
 
-const About = React.forwardRef(({ visibleHeader, visibleContents }, ref) => {
+const About = React.forwardRef(({ aboutVisibleabHeader, aboutVisibleContents }, ref) => {
   return (
     <div ref={ref}>
       <div className='aboutBackground'>
         <div className='header'>
-          <h1 className={`${visibleHeader ? 'lightSpeedInLeft' : 'hidden'}`}>ABOUT</h1>
-          <span className={`${visibleHeader ? 'lightSpeedInLeft' : 'hidden'}`}></span>
+          <h1 className={`${aboutVisibleabHeader ? 'lightSpeedInLeft' : 'hidden'}`}>ABOUT</h1>
+          <span className={`${aboutVisibleabHeader ? 'lightSpeedInLeft' : 'hidden'}`}></span>
         </div>
         <div className="aboutContents">
-          <div className={`aboutMe ${visibleContents ? 'fadeInLeft' : 'hidden'}`}>
+          <div className={`aboutMe ${aboutVisibleContents ? 'fadeInLeft' : 'hidden'}`}>
             <img className="myphoto" src="img/myphoto.jpg" alt='myphoto'/>
             <h2>HELLO!</h2>
             <p>UI / UX에 가치를 두고 작업하는 주니어 개발자입니다.<br/>
             호기심과 <span>도전 의식</span>이 강해 새로운 기술 습득에 거부감이 없고,<br/>
             <span>소통과 협업</span>을 중요시합니다.</p>
           </div>
-            <div className={`${visibleContents ? 'fadeInRight' : 'hidden'}`}>
+            <div className={`${aboutVisibleContents ? 'fadeInRight' : 'hidden'}`}>
               <Skills/>
               </div>
         </div>
