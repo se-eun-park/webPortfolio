eef4b1b001224a374592a40c8df1b431b02be255
박세은 <seeunbag30@gmail.com>
2023-11-26 22:22:18 +0900

    Update Code : 스킬 요소 생성

--- src/about.js
+++ src/about.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './style/about.css';
+import Skills from './skills';
 
 const About = React.forwardRef((props, ref) => {
   return (
@@ -17,6 +18,7 @@ const About = React.forwardRef((props, ref) => {
             호기심과 <span>도전 의식</span>이 강해 새로운 기술 습득에 거부감이 없고,<br/>
             <span>소통과 협업</span>을 중요시합니다.</p>
           </div>
+          <Skills/>
         </div>
       </div>
     </div>
--- src/skills.jsx
+++ src/skills.jsx
@@ -8,8 +8,65 @@ const skills = () => {
                 <span className='skillName'><p>HTML</p></span>
                 <div className='gageBar'>
                     <span className='gageBackground'><p>70%</p></span>
-                    <span className='gage'></span>
+                    <span className='gage'style={{ width: '70%' }}></span>
                 </div>
+                
+            </div>
+            <div className='skillBox'>
+                <span className='skillName'><p>CSS</p></span>
+                <div className='gageBar'>
+                    <span className='gageBackground'><p>70%</p></span>
+                    <span className='gage'style={{ width: '70%' }}></span>
+                </div>
+                
+            </div>
+            <div className='skillBox'>
+                <span className='skillName'><p>JavaScript</p></span>
+                <div className='gageBar'>
+                    <span className='gageBackground'><p>60%</p></span>
+                    <span className='gage'style={{ width: '60%' }}></span>
+                </div>
+                
+            </div>
+            <div className='skillBox'>
+                <span className='skillName'><p>React</p></span>
+                <div className='gageBar'>
+                    <span className='gageBackground'><p>60%</p></span>
+                    <span className='gage'style={{ width: '60%' }}></span>
+                </div>
+                
+            </div>
+            <div className='skillBox'>
+                <span className='skillName'><p>Node.js</p></span>
+                <div className='gageBar'>
+                    <span className='gageBackground'><p>40%</p></span>
+                    <span className='gage'style={{ width: '40%' }}></span>
+                </div>
+                
+            </div>
+            <div className='skillBox'>
+                <span className='skillName'><p>Python</p></span>
+                <div className='gageBar'>
+                    <span className='gageBackground'><p>50%</p></span>
+                    <span className='gage'style={{ width: '50%' }}></span>
+                </div>
+                
+            </div>
+            <div className='skillBox'>
+                <span className='skillName'><p>GitHub</p></span>
+                <div className='gageBar'>
+                    <span className='gageBackground'><p>50%</p></span>
+                    <span className='gage'style={{ width: '50%' }}></span>
+                </div>
+                
+            </div>
+            <div className='skillBox'>
+                <span className='skillName'><p>Figma</p></span>
+                <div className='gageBar'>
+                    <span className='gageBackground'><p>70%</p></span>
+                    <span className='gage'style={{ width: '70%' }}></span>
+                </div>
+                
             </div>
         </div>
 
